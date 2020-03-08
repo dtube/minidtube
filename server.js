6d0bf75fb8ec11c12fd02bdb3675e5b77adf4aa5
Adrien M <adrien@d.tube>
2020-03-09 00:35:34 +0100

    make google less unhappy

--- server.js
+++ server.js
@@ -228,10 +228,10 @@ function handleChainData(author, permlink, video, cb) {
     }
 
     var html = ''
-    html += '<iframe src="https://emb.d.tube/#!/'+author+'/'+permlink+'/true" width="480" height="270" frameborder="0" scrolling="no" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>'
+    html += '<div class="fluidMedia"><iframe src="https://emb.d.tube/#!/'+author+'/'+permlink+'/true" frameborder="0" scrolling="no" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div>'
     var title = video.json.title || video.json.info.title
     html += '<h1>'+title+'</h1>'
-    html += '<h2>Author: '+video.author+'</h2>'
+    html += '<h2>Author: <a href="/c/'+video.author+'">'+video.author+'</a></h2>'
 
     var description = null
     if (video.json.description)
